b5369aa2cd80e809c37d068ec49ebb390b5a5927
Youssef <benlarbi1youssef@gmail.com>
2022-10-19 11:44:49 +0100

    ADD folder/file stagiare.css

--- src/components/Stagiaire.js
+++ src/components/Stagiaire.js
@@ -7,13 +7,13 @@ function Stagiaire({id,nom,prenom,modules}) {
 	// desruction Object :
 	// const {id,nom,prenom,modules} = props ; 
 	return (
-		<ul>
+		<ul style={{listStyle: "circle",margin:0 }}>
 			<li>ID : {id}</li>
 			<li>Nom : {nom.toUpperCase()}</li>
 			<li>Prenom : {prenom.toUpperCase()}</li>
 			<li>
 				<h3>Listes Modules : </h3>
-				<ul>
+				<ul style={{listStyle: "disc" }}>
 					{/* {modules.map(function (element) {
 						return <li>{element}</li>;
 					})} */}
