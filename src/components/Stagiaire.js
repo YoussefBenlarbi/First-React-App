8edc12c31b450cbec3a62f8c69b74855556a4da1
Youssef <benlarbi1youssef@gmail.com>
2022-10-19 10:10:47 +0100

    function Map

--- src/components/Stagiaire.js
+++ src/components/Stagiaire.js
@@ -1,15 +1,26 @@
-
-function Stagiaire (){
-    const id =1 ;
-    const nom ="Errahmani" ;
-    const prenom ='maryam' ;
-    const modules =["POO","React","Laravel","SQL"];
-    return (<ul>
-        <li>ID : {id}</li>
-        <li>Nom : {nom.toUpperCase()}</li>
-        <li>Prenom : {prenom.toUpperCase()}</li>
-        <li>Modules : {modules[0]} {modules[1].toUpperCase()} {modules[2].toUpperCase()} {modules[3]}</li>        
-    </ul>)
+function Stagiaire() {
+	const id = 1;
+	const nom = 'Errahmani';
+	const prenom = 'maryam';
+	const modules = ['POO', 'React', 'Laravel', 'SQL', 'Back End'];
+	return (
+		<ul>
+			<li>ID : {id}</li>
+			<li>Nom : {nom.toUpperCase()}</li>
+			<li>Prenom : {prenom.toUpperCase()}</li>
+			<li>
+				<h3>Listes Modules : </h3>
+				<ul>
+					{/* {modules.map(function (element) {
+						return <li>{element}</li>;
+					})} */}
+					{modules.map((element) => 
+						<li>{element}</li>
+					)}
+				</ul>
+			</li>
+		</ul>
+	);
 }
 
-export default Stagiaire ;
+export default Stagiaire;
